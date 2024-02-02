90f4c4ca9465b72be0c6e481fc8c4fd1c70850dc
Mariklimova <mariku22@yandex.ru>
2024-02-02 22:00:59 +0300

    addition

--- src/helper/validation.js
+++ src/helper/validation.js
@@ -1,5 +1,8 @@
 function IsValidUserId(req, _res, next) {
     const { id } = req.params;
+    if(isNaN(id))throw new Error('id not number');
     if (id <= 0) throw new Error('id <= 0')
     next();
 }
+
+module.exports = {IsValidUserId}
